b74ff1fd83cdc8c09d822b98e7d4bb143576b5fb
Diego Rubin <drubin@caiena.net>
2013-05-03 14:05:20 -0300

    Adicionando dominio nos links locais do site.

--- vortex/src/vortex_extractdata.erl
+++ vortex/src/vortex_extractdata.erl
@@ -3,7 +3,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--export([getpage/1, getlinks/1]).
+-export([getpage/1, getlinks/1, getlinks/2]).
 
 getpage(Uri) ->
   inets:start(),
@@ -16,13 +16,15 @@ getpage(Uri) ->
 
   {ok, Body}.
 
-getlinks(Uri) ->
+getlinks(Uri) -> getlinks(Uri, []).
+
+getlinks(Uri, _Domains) ->
   {ok, Page} = getpage(Uri),
   Result = re:run(Page,"<a.*?href=['\"](.*?)['\"].*?>",[global, {capture, [1], list}]),
   
   case Result of
     {match, Links} ->
-      Links;
+      put_domain_in_local_paths(Links, Uri);
     nomatch ->
       [];
     _Else ->
@@ -30,9 +32,38 @@ getlinks(Uri) ->
   end.
 
 %
+% Private functions
+%
+%exlude_in_links(Links, Domains) ->
+%  Links.
+
+put_domain_in_local_paths(Links, Uri) ->
+
+  Result = re:run(Uri, "^(https?://[0-9a-zA-Z-.]+)/?",[{capture,[1],list}]),
+  {match, [Domain]} = Result,
+
+  put_domain_in_local_paths(Links, string:concat(Domain, "/"), []).
+
+put_domain_in_local_paths([], _, NewLinks) ->
+  NewLinks;
+put_domain_in_local_paths(Links, Domain, NewLinks) ->
+
+  [Link|RestLinks] = Links,
+
+  Result = re:run(Link, "^(/.*)",[{capture,[1],list}]),
+
+  case Result of
+    {match, Paths} ->
+      [Path|_] = Paths,
+      put_domain_in_local_paths(RestLinks, Domain, [[string:concat(Domain, Path)]|NewLinks]);
+    _Else ->
+      put_domain_in_local_paths(RestLinks, Domain, [Link|NewLinks])
+  end.
+
+%
 % tests
 %
 getpage_test_() ->
-  Body = getpage("http://google.com"),
+  _Body = getpage("http://google.com"),
   ok.
 
