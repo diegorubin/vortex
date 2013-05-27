bc7e5087bd4d3272afaf5c044fe4161d60a31e59
Diego Rubin <rubin.diego@gmail.com>
2013-05-27 11:55:04 -0300

    Metodo de indexacao simples para testes iniciais.

--- src/vortex_indexer.erl
+++ src/vortex_indexer.erl
@@ -7,7 +7,7 @@ index(Page) ->
   CleanedPage = clean(Page),
 
   Words = string:tokens(CleanedPage, ";,. ?"),
-  count([ string:to_lower(X) || X <- Words ]).
+  count(lists:sort([ string:to_lower(X) || X <- Words ])).
   
 
 % - clean
@@ -18,5 +18,29 @@ clean(Page) ->
 % - count
 % - Pontua as palavras
 count(Words) ->
-  Words.
+
+  count(Words, []).
+
+count([], Counteds) ->
+  Counteds;
+
+count(Words, Counteds) ->
+
+  [Word|_] = Words,
+
+  NewCounteds = lists:append(Counteds, [{Word, count_tokens(Word, Words)}]),
+
+  % - Remove elementos ja contados.
+  RestWords = lists:dropwhile(fun(Elem) -> Elem =:= Word end, Words),
+
+  count(RestWords, NewCounteds).
+
+count_tokens(Word, Words) ->
+  count_tokens(Word, Words, 0).
+
+count_tokens(Word, [Head|Rest], Total) when Word =:= Head ->
+  count_tokens(Word, Rest, Total + 1);
+count_tokens(_Word, _Words, Total) ->
+  Total.
+  
 
