6a0ed52e19525cac5fb3a7cb1642190e873f97fa
Diego Rubin <rubin.diego@gmail.com>
2013-07-30 21:45:19 -0300

    Testes funcionando, mock nao.

--- test/vortex_extractdata_tests.erl
+++ test/vortex_extractdata_tests.erl
@@ -2,15 +2,33 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-increment(N) -> N + 1.
+%getpage_test_() ->
+%  
+%  [{"get accessible page",
+%    fun() ->
+%      meck:new(httpc),
+%      meck:expect(httpc, request, fun() -> "<html></html>" end),
+%      Result = vortex_extractdata:getpage("http://diegorubin.com"),
+%      meck:unload(httpc),
+%
+%      ?assertEqual(
+%        {ok, 
+%          {statuscode, "200"},
+%          {contenttype, ""},
+%          {content, "<html></html>"}
+%        }, 
+%        Result)
+%    end}].
 
-getpage_test() ->
-  [{"inc by 0",
-    fun () ->
-        ?assertEqual(1, increment(0))
-    end},
-   {"inc by 1",
-    ?_test(?assertEqual(2, increment(1)))}].
+getlinks_test_() ->
+  
+  [{"get links from page",
+    fun() ->
+      Result = vortex_extractdata:getlinks("http://diegorubin.com/admin"),
 
+      ?assertEqual(
+        [["http://diegorubin.com/admin/#"]],
+        Result)
+    end}].
 
 
