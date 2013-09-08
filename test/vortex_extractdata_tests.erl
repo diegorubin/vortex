7b158d462a1651de6dc9de964d4d897734495702
Diego Rubin <rubin.diego@gmail.com>
2013-09-08 18:03:44 -0300

    [Teste Nao Passando] - Recuperando link relativo.
    Link relativo de arquivos estão errado:
    
    Exemplo -> http://teste.com/test.jpg em arquivo index.html
               está vindo http://teste.com/index.html/test.jpg

--- test/vortex_extractdata_tests.erl
+++ test/vortex_extractdata_tests.erl
@@ -6,11 +6,24 @@ getlinks_test_() ->
   
   [{"get links from page",
     fun() ->
-      Result = vortex_extractdata:getlinks("http://diegorubin.com/admin"),
+      Result = vortex_extractdata:getlinks("http://arquivos.diegorubin.com/vortex_test.html"),
 
       ?assertEqual(
-        [["http://diegorubin.com/admin/#"]],
+        [["http://arquivos.diegorubin.com/teste.jpg"],
+         ["http://diegorubin.com"],
+         ["http://arquivos.diegorubin.com/"]],
         Result)
     end}].
 
+getlinks_and_exclude_domain_test_() ->
+  
+  [{"get links from page and exclude domain",
+    fun() ->
+      Result = vortex_extractdata:getlinks("http://arquivos.diegorubin.com/vortex_test.html", ["diegorubin.com"]),
+
+      ?assertEqual(
+        [["http://arquivos.diegorubin.com/"],
+         ["http://arquivos.diegorubin.com/teste.jpg"]],
+        Result)
+    end}].
 
