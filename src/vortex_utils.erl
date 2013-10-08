23beac39eaa97c806bd5c87e2494f2fa286e4a35
Diego Rubin <rubin.diego@gmail.com>
2013-10-08 20:18:51 -0300

    Tentando remover acentos.

---
+++ src/vortex_utils.erl
@@ -0,0 +1,11 @@
+%% -*- coding: utf-8 -*-
+%% @author Diego Rubin <rubin.diego@gmail.com>
+%% @copyright 2013 Diego Rubin.
+
+-module(vortex_utils).
+-author('Diego Rubin <rubin.diego@gmail.com>').
+
+-export([remove_accent/1]).
+
+remove_accent(String) -> String.
+
